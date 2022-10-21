a83271b37b61cacb25f2091fb237cbbab1913234
maithi-trandiep <thi.tdm04@gmail.com>
2022-10-21 20:47:53 +0200

    Some changes

--- src/scripts/clock.js
+++ src/scripts/clock.js
@@ -1,4 +1,4 @@
-function clock(event, el) {
+function openTab(event, el) {
     let i, tabContent, tabLinks;
     // clear the previous clicked content
     tabContent = document.getElementsByClassName("tab-content")
@@ -21,9 +21,11 @@ function setClock() {
     const handHour = document.querySelector(".hand-hour")
     const handMin = document.querySelector(".hand-min")
     const handSecond = document.querySelector(".hand-second")
+    let timeRef = document.getElementById("digitalClock")
 
     const now = new Date()
 
+    // analog clock
     const seconds = now.getSeconds()
     const secondsPosition = ((seconds / 60) * 360) + 90
     handSecond.style.transform = `rotate(${secondsPosition}deg)`
@@ -42,13 +44,14 @@ function setClock() {
         document.getElementById("timeRef").innerHTML = 'AM'
     }
 
-    hoursText = (hours < 10) ? "0" + hours : hours;
-    minsText = (mins < 10) ? "0" + mins : mins;
-    secondsText = (seconds < 10) ? "0" + seconds : seconds;
+    // digital clock
+    let hoursText = (hours < 10) ? "0" + hours : hours;
+    let minsText = (mins < 10) ? "0" + mins : mins;
+    let secondsText = (seconds < 10) ? "0" + seconds : seconds;
 
-    const timeText = hoursText + ":" + minsText + ":" + secondsText
-    document.getElementById("digitalClock").innerText = timeText
-    document.getElementById("digitalClock").textContent = timeText
+    let timeText = hoursText + ":" + minsText + ":" + secondsText
+    timeRef.innerText = timeText
+    timeRef.textContent = timeText
 }
 
 setInterval(setClock, 1000)
