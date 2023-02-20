2537b1a56a68991e232728b1c69fdd972eff7e7c
aliciasaci <lyciiasci@gmail.com>
2023-02-20 17:44:50 +0100

    responsivite tictactoe

--- scripts/Theme.js
+++ scripts/Theme.js
@@ -3,22 +3,27 @@ export function setTheme(theme) {
   if (theme == "dark") {
     //dark mode
     let window = document.querySelector(".window");
-    let windowUpperBar = document.querySelector(".window-upper-btns")
+    let windowUpperBar = document.querySelector(".window-upper-btns");
     let startingPage = document.querySelector("#starting-page");
-    let paramTitle =   document.querySelector(".param-title");
+    let paramTitle = document.querySelector(".param-title");
+    let windowReduceBtn = document.querySelector(".fa-minus");
 
     window.removeAttribute("class");
     window.setAttribute("class", "window center darkglass");
 
-    if(windowUpperBar){
+    if (windowUpperBar) {
       windowUpperBar.style.backgroundColor = "rgba(14, 14, 14, 0.7)";
     }
-    if(paramTitle){
+    if (paramTitle) {
       paramTitle.style.color = "#f7f7f7"
     }
-    if(startingPage){
+    if (startingPage) {
       startingPage.style.backgroundColor = "#1d1d1d";
     }
+    if (windowReduceBtn) {
+      windowReduceBtn.style.color = "#FFF";
+    }
+
     document.body.style.backgroundImage = "url('assets/bg_9.jpg')";
   } else if (theme == "light") {
     //light mode
